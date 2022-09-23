ad14e82f2b25bbb3121aa7b25ea7bafcdd2b8b42
santiagomillan <santiagomillan@usantotomas.edu.co>
2022-09-23 11:37:31 -0500

    letras tipo matrix.

--- js/main.js
+++ js/main.js
@@ -1,16 +1,32 @@
 const canvas = document.querySelector("#canvas");
+const ctx = canvas.getContext("2d");
+// canvas.width = document.body.clientWidth
+// canvas.height = document.body.clientHeight
 
-canvas.width = document.body.clientWidth
-canvas.height = document.body.clientHeight
+const w = document.body.clientWidth;
+const h = document.body.clientHeight;
+
+canvas.width = w;
+canvas.height = h;
 
 const position = Array(300).join(0).split("");
 
-const ctx = canvas.getContext("2d");
-// console.log(ctx)
+
+// console.log(position)
 
 const initMatrix = () => {
-    ctx.fillStyle = "#00ff00";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "rgba(0,15,2,0.15)";
+    ctx.fillRect(0, 0, w, h);
+
+    ctx.fillStyle = '#4caf50'
+    ctx.font = "11pt"
+
+    position.map((y, index) => {
+        const text = String.fromCharCode(1e3 + Math.random() * 30);
+        const x = (index * 15) + 15
+        canvas.getContext('2d').fillText(text, x, y)
+        y > 100 + Math.random() * 1e5 ? position[index] = 0 : position[index] = y + 15
+    })
 }
 
-setInterval(initMatrix, 70);
+setInterval(initMatrix, 80);
